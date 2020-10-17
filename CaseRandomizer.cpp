e8a608da7357bd42a28b63deb798985bd386f1de
Nguyen Duy Thanh <yohowdoyoudo@gmail.com>
2020-10-18 00:44:24 +0300

    updated CaseRandomizer.cpp

--- CaseRandomizer.cpp
+++ CaseRandomizer.cpp
@@ -20,11 +20,18 @@ Test  |               Result
 #include <iostream>
 #include <cctype>
 #include <cstring>
+#include <cstdio>
+
 using namespace std;
+
+//global variable declaration
+char prevChar1;
+char prevChar2;
 
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 //main function
 int main()
 {
 
+  return 0;
 }
