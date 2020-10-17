cd26da58f9d1d00d5044b46c42515f23d1e77f6b
Nguyen Duy Thanh <yohowdoyoudo@gmail.com>
2020-10-18 02:36:43 +0300

    updated CaseRandomizer.cpp

--- CaseRandomizer.cpp
+++ CaseRandomizer.cpp
@@ -24,14 +24,34 @@ Test  |               Result
 
 using namespace std;
 
+//function prototype(s)
+char CaseRandomizer(char str);
+
 //global variable declaration
 char prevChar1;
 char prevChar2;
+const N = 1000;
 
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 //main function
-int main()
+int main(void)
 {
+  //local variable declaration
+  char str[N];
 
+  //code
+  cout << "Input string:";
+  cin >> str[N];
+  cout << "Sponge-text:"
   return 0;
-}
+}//end main
+
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+//fuction(s) declaration
+char CaseRandomizer(char str);
+{
+	//local variable declaration
+
+	//code
+
+}//end function_name
