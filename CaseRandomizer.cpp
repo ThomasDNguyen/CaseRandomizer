e8fa6e34fdb2dd832b2feb1c2972ed5b73f7ebc7
Nguyen Duy Thanh <yohowdoyoudo@gmail.com>
2020-10-17 23:39:30 +0300

    updated README and CaseRandomizer.cpp

--- CaseRandomizer.cpp
+++ CaseRandomizer.cpp
@@ -1,7 +1,30 @@
+/*
+Objective: randomize uppercase/lowercase characters of input string.
+For example:
+
+Test  |               Result
+------------------------------------------------
+					|
+					|
+					|
+------------------------------------------------
+					|
+					|
+					|
+------------------------------------------------
+					|
+					|
+					|
+*/
+
 #include <iostream>
+#include <cctype>
+#include <cstring>
 using namespace std;
 
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+//main function
 int main()
 {
-  
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-blabla
+I/ Intro:
+- This is a Windows application that helps you randomize the uppercase/lowercase characters for a sponge-text meme.
+
+II/ Install/Setup:
+<NULL>
+
+III/ How_to:
+<NULL>
