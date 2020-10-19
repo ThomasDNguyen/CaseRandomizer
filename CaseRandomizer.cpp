4d9cae70500487c2f32d43dd94a317b5a9371124
Nguyen Duy Thanh <yohowdoyoudo@gmail.com>
2020-10-19 23:22:35 +0300

    installed and modified crustify -> apply Atom beautify -> working formatter for project

--- CaseRandomizer.cpp
+++ CaseRandomizer.cpp
@@ -1,21 +1,21 @@
 /*
-Objective: randomize uppercase/lowercase characters of input string.
-For example:
+   Objective: randomize uppercase/lowercase characters of input string.
+   For example:
 
-Test  |               Result
-------------------------------------------------
-					|
-					|
-					|
-------------------------------------------------
-					|
-					|
-					|
-------------------------------------------------
-					|
-					|
-					|
-*/
+   Test				    | Result
+   ------------------------------------------------
+   					|
+   					|
+   					|
+   ------------------------------------------------
+   					|
+   					|
+   					|
+   ------------------------------------------------
+   					|
+   					|
+   					|
+ */
 
 #include <iostream>
 #include <cctype>
@@ -36,22 +36,22 @@ const N = 1000;
 //main function
 int main(void)
 {
-  //local variable declaration
-  char str[N];
+        //local variable declaration
+        char str[N];
 
-  //code
-  cout << "Input string:";
-  cin >> str[N] >> endl;
-  cout << "Sponge-text:"
-  return 0;
+        //code
+        cout << "Input string:";
+        cin >> str[N] >> endl;
+        cout << "Sponge-text:"
+                return 0;
 }//end main
 
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 //fuction(s) declaration
 char CaseRandomizer(char str);
 {
-	//local variable declaration
+        //local variable declaration
 
-	//code
+        //code
 
 }//end function_name
